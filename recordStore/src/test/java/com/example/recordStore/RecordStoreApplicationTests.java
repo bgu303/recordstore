c78ab86834f2a9c4d6f3c0066d49547c7587af12
Hanna Suojanen <hanna.s89@gmail.com>
2022-10-17 11:50:33 +0300

    more testing

---
+++ recordStore/src/test/java/com/example/recordStore/RecordStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recordStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
