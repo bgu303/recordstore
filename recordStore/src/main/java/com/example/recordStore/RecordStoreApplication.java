4b3493c58dfad066de1b6991cd9186a0c6337823
Hanna Suojanen <hanna.s89@gmail.com>
2022-10-19 13:11:51 +0300

    skeleton ready

--- recordStore/src/main/java/com/example/recordStore/RecordStoreApplication.java
+++ recordStore/src/main/java/com/example/recordStore/RecordStoreApplication.java
@@ -20,9 +20,28 @@ public class RecordStoreApplication {
 	@Bean
 	public CommandLineRunner demo(RecRepository repository) {
 		return (args) -> {
-			Rec record1 = new Rec("AAAAAA", "LP", "LANDSCAPES", "DETOUR", "EX",	"EX", 25, "r3521240");
+			repository.deleteAll();
+			Rec record1 = new Rec("007 / THE SCENE", "LP", "LANDSCAPES", "DETOUR", "EX", "EX", 25, "r3521240");
+            Rec record2 = new Rec("10cc", "LP", "ART FOR ARTS SAKE", "J&B RECORDS/MERCURY", "EX", "VG+", 4, "r636809");
+            Rec record3 = new Rec("10cc" , "LP", "HOW DARE YOU", "MERCURY", "VG", "VG", 3, "r2236095");
+            Rec record4 = new Rec("49th PARALLEL", "LP", "49th PARALLEL", "bootleg", "EX", "EX", 30, "r3178159");
+            Rec record5 = new Rec("5th DIMENSION", "LP", "LOVE'S LINES, ANGELS AND RHYME", "BELL", "EX", "EX", 3, "r402603");
+            Rec record6 = new Rec("999", "12\"", "WAITING / ACTION (12\")", "LABRITAIN", "VG+", "VG", 8, "r2120964");
+            Rec record7 = new Rec("A CERTAIN RATIO", "12\"", "DO THE DU (CASSE)", "FACTORY", "EX", "EX", 18, "r13050446");
+            Rec record8 = new Rec("A CERTAIN RATIO", "12\"", "FLIGHT(12\")", "FACTORY", "EX-", "VG+", 15, "r3915466");
+            Rec record9 = new Rec("ABC", "LP", "LEXICON OF LOVE", "VERTIGO", "EX", "VG+", 5, "r114525");
+            Rec record10 = new Rec("AC/DC", "12\"", "WHO MADE WHO (PROMO)", "ATLANTIC", "EX", "VG", 10, "r6967323");
 			
-			repository.save(record1);
+            repository.save(record1);
+            repository.save(record2);
+            repository.save(record3);
+            repository.save(record4);
+            repository.save(record5);
+            repository.save(record6);
+            repository.save(record7);
+            repository.save(record8);
+            repository.save(record9);
+            repository.save(record10);
 			
 		};
 	}
--- recordStore/src/main/java/com/example/recordStore/web/RecordStoreEndPoints.java
+++ recordStore/src/main/java/com/example/recordStore/web/RecordStoreEndPoints.java
@@ -1,8 +1,36 @@
 package com.example.recordStore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.recordStore.domain.Rec;
+import com.example.recordStore.domain.RecRepository;
 
 @Controller
 public class RecordStoreEndPoints {
 
-}
+	@Autowired
+	private RecRepository repository;
+	
+	@GetMapping("/records") 
+	public String recordController(Model model) {
+		model.addAttribute("records", repository.findAll());
+		return "recordlist";
+	}
+	
+	@GetMapping("/addrecord")
+	public String addRecord(Model model) {
+		model.addAttribute("record", new Rec());
+		return "addrecord";	
+	}
+	
+	@PostMapping("/saverecord")
+	public String saveRecord(Rec record) {
+		repository.save(record);
+		return "redirect:records";
+	}
+	}
+
